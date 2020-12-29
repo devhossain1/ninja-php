453abbcdcd5165d19898deddfa2cd0a819c3a12a
hossain377 <hossainahmed377@gmail.com>
2020-12-29 10:20:14 +0600

    #32.deleting record

--- details.php
+++ details.php
@@ -1,6 +1,19 @@
 <?php 
 include('config/db_connect.php');
 
+if(isset($_POST['delete'])){
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+    $sql = "DELETE FROM ninjas WHERE id = $id_to_delete";
+    
+    if(mysqli_query($conn, $sql)){
+        //success
+        header('Location: index.php');
+    } {
+        //failure
+        echo 'query error:' . mysqli_error($conn);
+    }
+}
+
 //check get request in id paramiter
 if(isset($_GET['id'])){
   $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -33,7 +46,13 @@ $result = mysqli_query($conn,$sql);
        <p><?php echo date($pizza['created_at']); ?></p>
        <h5>ingredients:</h5>
        <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+<!--       Delete Forms-->
+        <form action="details.php" method="POST">
+            <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+            <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+        </form>
          
+             
    <?php else: ?>
     <h5>no such pizza exist!</h5>
    <?php endif; ?>
